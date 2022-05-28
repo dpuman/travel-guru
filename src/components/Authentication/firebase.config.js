c9c0b9978336cda972bdeffa3f64c5589437684d
Dpu <dpumail.in@gmail.com>
2022-05-28 16:10:04 +0600

    initial and done with deployment

---
+++ src/components/Authentication/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDJ6FUtcQM1B0mk9L0d5gB-eCFfdyyyZd8",
+    authDomain: "travel-guru-ca1d3.firebaseapp.com",
+    projectId: "travel-guru-ca1d3",
+    storageBucket: "travel-guru-ca1d3.appspot.com",
+    messagingSenderId: "660749197067",
+    appId: "1:660749197067:web:b57d710f94a2dd18504c0e"
+};
+export default firebaseConfig;
